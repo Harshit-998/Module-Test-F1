1eea14f582acae526c07452ed578c82d7ab09890
Harshit Yadav <61971255+Harshit-998@users.noreply.github.com>
2022-12-30 23:57:25 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,12 @@
+function submitted() {
+  let fName = document.getElementById("First-Name").value;
+  let lName = document.getElementById("Last-Name").value;
+  let email = document.getElementById("Email-Address").value;
+  console.log(fName, lName, email);
+
+  if (fName && lName && email) {
+    alert("Form Submitted Successfully!!");
+  } else {
+    alert("Please fill all fields");
+  }
+}
